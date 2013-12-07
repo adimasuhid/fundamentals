fb8f7e0894452ab1ebdaaf04eab95b4785d3a5e0
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-12-07 22:12:17 +0800

    Cool Commits

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 (function($){
+    //Data
+    var books = [{title: "Public Speaking Graciously", releaseDate: "2020", keywords: "Public Speaking"},
+                {title: "Knowing dusk and dawn", releaseDate: "2020", keywords: "Personal Development"},
+                {title: "Code it Like its Hot", releaseDate: "2020", keywords: "Programming"},];
+
+    //Models
     var Book = Backbone.Model.extend({
         defaults: {
-            coverImage: "img/placeholder.png",
+            coverImage: "placeholder.png",
             title: "Some title",
-            author: "John Doe",
+            author: "Ace Dimasuhid",
             releaseDate:"2012",
             keywords: "Javascript Programming"
         }
     });
 
+    //Collections
+    var Library = Backbone.Collection.extend({
+        model: Book
+    });
+
+    //Views
     var BookView = Backbone.View.extend({
         tagName: "div",
         className: "bookContainer",
@@ -22,16 +34,62 @@
         }
     });
 
-    var book = new Book({
-        title: "Learning Javascript the Right Way",
-        author: "Ace Dimasuhid",
-        releaseDate: "2012",
-        keywords: "JavaScript Programming"
+    var LibraryView = Backbone.View.extend({
+        el: $("#books"),
+
+        events: {
+            "click #add":"addBook"
+        },
+
+        initialize: function(){
+            this.collection = new Library(books);
+            this.render();
+
+            this.collection.on("add", this.renderBook, this);
+        },
+
+        render: function(){
+            var that = this;
+            this.collection.each(function(item){
+                that.renderBook(item);
+            })
+        },
+
+        renderBook: function(item){
+            var bookView = new BookView({
+                model:item
+            });
+            this.$el.append(bookView.render().el);
+        },
+
+        addBook:function(e){
+            e.preventDefault();
+            var formData = {};
+            var that = this;
+
+            $("#addBook div").children("input").each(function(i, el){
+                if ($(el).val() !== "") {
+                    var value = $(el).val();
+
+                    formData[el.id] = that.formValue(el.id,value);
+                }
+            });
+
+            books.push(formData);
+
+            this.collection.add(new Book(formData));
+        },
+
+        formValue: function(id, value){
+            if (id == "coverImage") {
+                return value.split('\\').pop();
+            } else{
+                return value
+            }
+        }
     });
 
-    bookView = new BookView({
-        model:book
-    });
+    //Implementations
+    var libraryView = new LibraryView();
 
-    $("#books").html(bookView.render().el);
 })(jQuery);
